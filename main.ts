d0335af4e3f3034f6980bdb2812a8f9b58d8cb29
Anastasiia Lekvina <146827165+AnastasiiaLevkina@users.noreply.github.com>
2024-10-28 16:05:49 +0100

    Actualització main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,22 @@
+function check_temperature () {
+    if (input.temperature() > 22) {
+        basic.showString("calor")
+    } else {
+        basic.showString("fred")
+    }
+}
+function alarm_clock () {
+    if (input.lightLevel() > 200) {
+        music.setBuiltInSpeakerEnabled(true)
+        music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Birthday), music.PlaybackMode.UntilDone)
+    } else if (input.lightLevel() < 50) {
+        music.setBuiltInSpeakerEnabled(true)
+        music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Funeral), music.PlaybackMode.UntilDone)
+    } else {
+        music.setBuiltInSpeakerEnabled(false)
+    }
+}
 basic.forever(function () {
-	
+    check_temperature()
+    alarm_clock()
 })
